4f825e3af8e695c88026698a863d1194a5ccce7e
MvDUKZA <m@vd.gg>
2025-04-15 09:58:36 +0100

    Update windows_cleanup.ps1

--- windows_cleanup.ps1
+++ windows_cleanup.ps1
@@ -1,18 +1,6 @@
-# Create the .reg file to enable Disk Cleanup for Previous Windows Installation(s)
-$regContent = @"
-Windows Registry Editor Version 5.00
+# Write the required registry key directly to enable cleanup of Previous Windows Installations
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Previous Installations" `
+                 -Name "StateFlags0001" -Value 2 -Type DWord
 
-[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Previous Installations]
-"StateFlags0001"=dword:00000002
-"@
-
-$regPath = "$env:TEMP\enable_windows_old_cleanup.reg"
-
-# Save the .reg file with proper encoding (UTF-16 LE)
-$regContent | Out-File -FilePath $regPath -Encoding Unicode
-
-# Import the registry file
-reg import "$regPath"
-
-# Run Disk Cleanup for preset #1
+# Now run Disk Cleanup with sagerun preset 1
 cleanmgr /sagerun:1
