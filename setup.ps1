f8a80b73fe3470ead31c5843a17f137ca935371d
MvDUKZA <m@vd.gg>
2025-08-26 13:28:11 +0100

    Update

--- setup.ps1
+++
@@ -1,7 +0,0 @@
-# Setup WinRM for Packer
-Write-Output "Configuring WinRM for Packer..."
-winrm quickconfig -q
-winrm set winrm/config/service/Auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true
-Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $true
